0c05c11d8bd2750a3a97023ee8e22e520e1e2b34
Milagros González <110262020+gonzalezmilagros@users.noreply.github.com>
2022-09-01 19:50:56 -0300

    1/9

--- js/products.js
+++ js/products.js
@@ -1,6 +1,14 @@
+let listaProductos = []
+let min = undefined //no está definida, o sea no está inicializada//
+let max = undefined
+
 function mostrarProductos(arrayProductos) {
+    document.getElementById("productos").innerHTML = "";
     for (const producto of arrayProductos) {
-        let contenido = `
+        producto.cost = parseInt(producto.cost);
+        if ((min == undefined && max == undefined) || (producto.cost >= min && producto.cost <= max) ||
+        (producto.cost >= min && max == undefined) || (producto.cost <= max && min == undefined)) {
+            let contenido = `
         <ul>
             <div class="container-lista-productos">
                 <div class="card-lista-productos">
@@ -24,22 +32,40 @@ function mostrarProductos(arrayProductos) {
         `;
         document.getElementById("productos").innerHTML += contenido;
     }
+ }
+       
 }
 
 
 document.addEventListener("DOMContentLoaded", function () {
     getJSONData(PRODUCTS_URL + localStorage.getItem("catID") + EXT_TYPE).then(resultado => {
-        // console.log(resultado);
         if (resultado.status == "ok") {
-            let listaProductos = resultado.data.products;
+            listaProductos = resultado.data.products;
             mostrarProductos(listaProductos);
-            // console.log(listaProductos)
         } else {
             alert("algo salió mal: " + resultado.data.products)
         }
     })
+
+
+    document.getElementById("filtrar").addEventListener("click", function(){
+        min = document.getElementById("rango-min").value != "" ? parseInt(document.getElementById("rango-min").value) : undefined;
+        max = document.getElementById("rango-max").value != "" ? parseInt(document.getElementById("rango-max").value) : undefined;
+        mostrarProductos(listaProductos);
+    })
+    
+    document.getElementById("limpiar").addEventListener("click", function() {
+        min = undefined;
+        max = undefined;
+        document.getElementById("rango-min").value = "";
+        document.getElementById("rango-max").value = "";
+        mostrarProductos(listaProductos);
+        
+    })
+
 })
 
 
 
 
+// parseInt: transforma strings numéricos en números
