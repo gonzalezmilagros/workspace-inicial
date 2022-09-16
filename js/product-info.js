2441609fb96cdc08e9cb10054ba5109c63302ac6
Milagros González <110262020+gonzalezmilagros@users.noreply.github.com>
2022-09-16 16:57:18 -0300

    16/9

--- js/product-info.js
+++ js/product-info.js
@@ -2,7 +2,8 @@ let dato1;
 let dato2;
 
 
-// No se como llamarle a esto, pero vamos a decir que se le atribuye a a los numeros lo que se debe imprimir en pantalla
+
+// No se como llamarle a esto, pero vamos a decir que se le atribuye  a los numeros lo que se debe imprimir en pantalla
 star = {
   datos : false,
 1:`<span class="fa fa-star checked"></span>
@@ -32,47 +33,64 @@ star = {
    <span class="fa fa-star checked"></span>`,
 }
 
-function showEverything(arrayComentarios){
+function showComments(){
+
+   // Se recorre el array y se muestran los comentarios de los productos
+
+   let htmlContentToAppend = "";
+for(let i = 0; i < dato2.length; i++){
+   let comment = dato2[i];
+
+       htmlContentToAppend += `
+       ${comment.user}  ${comment.dateTime}  ${star[comment.score]} <br>
+       ${comment.description} <br><br>
+       `
+   }
+
+   document.getElementById("comentarios").innerHTML = htmlContentToAppend;
+}
+
+
+function showInfo(){
 
     // Con innerHTML se imprime los objetos del json
    
-    document.getElementById("product-name").innerHTML = " " + dato1.name; 
-        document.getElementById("product-cost").innerHTML = "Precio " + dato1.currency + dato1.cost;
-        document.getElementById("product-description").innerHTML = "Descripción " + dato1.description;
-        document.getElementById("product-category").innerHTML = "Categoría " + dato1.category;
-        document.getElementById("product-soldCount").innerHTML = "Cantidad de vendidos " + dato1.soldCount;
-     
-        // Aquí tenes que poner las imagenes pedazo de gila, porfis hacelo antes del domingo!!!
+        document.getElementById("product-name").innerHTML = " " + dato1.name; 
+        document.getElementById("product-cost").innerHTML = " " + dato1.currency + dato1.cost;
+        document.getElementById("product-description").innerHTML = " " + dato1.description;
+        document.getElementById("product-category").innerHTML = " " + dato1.category;
+        document.getElementById("product-soldCount").innerHTML = " " + dato1.soldCount;
 
-        // Se recorre el array y se imprime los elementos del mismo
-
-        let htmlContentToAppend = "";
-    for(let i = 0; i < dato2.length; i++){
-        let comment = dato2[i];
-
-            htmlContentToAppend += `
-            ${comment.user}  ${comment.dateTime}  ${star[comment.score]} <br>
-            ${comment.description} <br><br>
-            `
-        }
-
-        document.getElementById("comentarios").innerHTML = htmlContentToAppend;
-    }
+      //Recorremos el array e imprimimos las imagenes en pantalla.
 
 
+      // for(let imagen of dato1.images){
+      //    let img = `
+      //    <div>
+      //        <div>`+ imagen.images[0] +`</div>
+      //        <div>`+ imagen.images[1] +`</div>
+      //        <div>`+ imagen.images[2] +`</div>
+      //        <div>`+ imagen.images[3] +`</div>
+      //    </div>    
+      //    `;
+      //    document.getElementById("images").innerHTML += img;
+      // }
+}
+   
+
+        
 
 fetch(PRODUCT_INFO_URL + localStorage.getItem("InfoID") + EXT_TYPE)
      .then(respuesta => respuesta.json())
      .then(result1 => {
         dato1 = result1;
-        showEverything()
-})
-
-fetch(PRODUCT_INFO_COMMENTS_URL + localStorage.getItem("InfoID") + EXT_TYPE)
-        .then(respuesta => respuesta.json())
-        .then(result2 => {
-            console.log(result2);
-            dato2 = result2;
-            showEverything()
+        console.log(result1);
+        fetch(PRODUCT_INFO_COMMENTS_URL + localStorage.getItem("InfoID") + EXT_TYPE)
+            .then(respuesta => respuesta.json())
+            .then(result2 => {
+               dato2 = result2;
+               showInfo()
+               showComments()
         })
+   })
 
