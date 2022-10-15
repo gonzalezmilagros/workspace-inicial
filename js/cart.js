0b4231b8e0da45e4476be5f6cb6c77dc1cd23198
Milagros González <110262020+gonzalezmilagros@users.noreply.github.com>
2022-10-15 17:47:41 -0300

    15/10

--- js/cart.js
+++ js/cart.js
@@ -1,9 +1,41 @@
 const USER_CART = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
+let listaCart = [];
 
-fetch(PRODUCT_INFO_URL + `25801` + EXT_TYPE)
-            .then(respuesta => respuesta.json())
-            .then(result => {
-               dato = result;
-               showComments();
-               // console.log(dato);
-})
+document.addEventListener("DOMContentLoaded", () => {
+   getJSONData(USER_CART).then(resultado => {
+         if (resultado.status == "ok") {
+            listaCart = resultado.data.articles;
+            cartProducts(listaCart);
+            // console.log(resultado);
+        } else {
+            alert("algo salió mal: " + resultado.data.articles)
+        }
+      })
+
+   var container = document.getElementById("carritoProduct")
+
+
+// -----------------------------------------------------------------------------------------------------------------------------//
+// Función que muestra los productos en el carrito.
+// -----------------------------------------------------------------------------------------------------------------------------//  
+
+
+const cartProducts = (data) => {
+   container.innerHTML += ''
+   for (pCart in data) {
+      container.innerHTML += `
+      <li class="list-group-item" aria-current="true">
+      ${data[pCart].name}    
+      ${data[pCart].unitCost}
+      </li>
+      `
+  }
+}
+
+});
+
+
+
+
+
+
