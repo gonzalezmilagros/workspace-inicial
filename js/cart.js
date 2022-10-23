a93b75b5390c576427c10b165649ccaa5258ba9b
Milagros González <110262020+gonzalezmilagros@users.noreply.github.com>
2022-10-22 22:03:04 -0300

    Cambios sobre el navbar y la página de cart.

--- js/cart.js
+++ js/cart.js
@@ -6,19 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
       if (resultado.status == "ok") {
          listaCart = resultado.data.articles;
          cartProducts(listaCart);
-         const myFuncion = () => {
-          let btnCantidad = document.getElementById("myInput");
-        
-          btnCantidad.addEventListener('click', function() {
-            document.getElementById("elSubtotal").innerHTML = btnCantidad.value*15200;
-            console.log();
-                    })
-        }
-         myFuncion();
+         inputSubtotal.oninput = function() {
+          mySubtotal.innerHTML = inputSubtotal.value * 15200;
+        };
         } else {
          alert("algo salió mal: " + resultado.data.articles)
       }
    })
+
 
    var container = document.getElementById("carritoProduct")
 
@@ -27,93 +22,108 @@ document.addEventListener("DOMContentLoaded", () => {
    // -----------------------------------------------------------------------------------------------------------------------------//
    // Función que muestra los productos en el carrito y el formulario de envío.
    // -----------------------------------------------------------------------------------------------------------------------------//  
-{/* <button class="btn btn-light boton-product" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">  */}
-   const cartProducts = (data) => {
-      container.innerHTML += ''
+   
+   
+   
+    const cartProducts = (data) => {
+    container.innerHTML += ''
       for (pCart in data) {
          container.innerHTML += `
-         <p>
-         
-  <div class="container list-group-item product">
-  <div class="row">
-    <div class="col-1 infoP">
-    <img src="${data[pCart].image}" alt="" class="img-cart">
-    </div>
-    <div class="col-2 infoP">
-    ${data[pCart].name}
-    </div>
-    <div class="col-2 infoP">
-    ${data[pCart].currency}${data[pCart].unitCost}
-    </div>
-    <div class="col-2 infoP">
-    <input type="number" id="myInput" oninput="myFuncion" min="1">
+
+        <div class="card" style="width: 14rem;">
+
+            <img class="card-img-top" src="${data[pCart].image}" alt="Card image cap">
+          <div class="card-body ">
+
+            <h5 class="card-title"><strong>${data[pCart].name}</strong></h5>
+
+            <p class="card-text">
+
+              <p>Precio: ${data[pCart].currency}${data[pCart].unitCost}</p><hr>
+
+              <p>Cantidad: <input type="number" id="inputSubtotal" min="1" style="width: 5rem;" placeholder="1"></p><hr>
+
+              <p">Subtotal: ${data[pCart].currency}<span id="mySubtotal"</span></p>
+
+            </p>
+          </div>
+
+        <div>
+          <button class="btn btn-primary position-absolute top-50 start-50 translate-middle" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Comprar</button>
+        </div>
+
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+
+        <div class="offcanvas-header">
+          <h4 class="offcanvas-title" id="offcanvasRightLabel">${data[pCart].name}</h4>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+
+        <div class="offcanvas-body">
+
+          <hr><h5>Tipos de envío</h5>
+
+            <div class="form-check form-switch">
+              <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+              <label class="form-check-label" for="flexSwitchCheckDefault">Premium 2 a 5 días(15%)</label>
+            </div>
+
+            <div class="form-check form-switch">
+              <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+              <label class="form-check-label" for="flexSwitchCheckDefault">Express 5 a 8 días(7%)</label>
+            </div>
+
+            <div class="form-check form-switch">
+              <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+              <label class="form-check-label" for="flexSwitchCheckDefault">Standard 12 a 15 días(5%)</label>
+            </div>
+    
+          <hr><h5>Dirección de envío</h5>
+
+          <div class="row g-3">
+
+            <div class="col-sm-10">
+              <input type="text" class="form-control" placeholder="Calle" aria-label="Ciudad">
+            </div>
+
+            <div class="col-sm-5">
+              <input type="text" class="form-control" placeholder="Número" aria-label="Estado">
+            </div>
+
+            <div class="col-sm-5">
+              <input type="text" class="form-control" placeholder="Esquina" aria-label="Código postal">
+            </div>
+    
+          </div>
+    
+          <div class="col-sm-5">
+
+            <hr><h5>Costo</h5>
+    
+              <div class="card card-body">
+              </div>
+              <div class="card card-body">
+              </div>
+              <div class="card card-body">
+              </div>
+
+          </div>
+    
+          <div class="col-sm-5">
+    
+            <hr><h5>Forma de pago</h5>
+    
+          </div>
+
       </div>
-      <p class="col-3 infoP" id="elSubtotal">
-      ${data[pCart].currency}
-        </p>
+    </div>
   </div>
-</div>
-  </button>
-</p>
-  <div class="card card-body">
-  <h5>Tipos de envío</h5>
-  <div class="form-check form-switch">
-    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-  <label class="form-check-label" for="flexSwitchCheckDefault">Premium 2 a 5 días(15%)</label>
-</div>
-<div class="form-check form-switch">
-  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-  <label class="form-check-label" for="flexSwitchCheckDefault">Express 5 a 8 días(7%)</label>
-</div>
-<div class="form-check form-switch">
-  <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-  <label class="form-check-label" for="flexSwitchCheckDefault">Standard 12 a 15 días(5%)</label>
-</div>
-<h5>Dirección de envío</h5>
-<div class="row g-3">
-  <div class="col-sm-5">
-    <input type="text" class="form-control" placeholder="Calle" aria-label="Ciudad">
-  </div>
-  <div class="col-sm-3">
-    <input type="text" class="form-control" placeholder="Número" aria-label="Estado">
-  </div>
-  <div class="col-sm-5">
-    <input type="text" class="form-control" placeholder="Esquina" aria-label="Código postal">
-  </div>
-</div>
-</div>
 </div>
 `
-   }
-}  
-
+       }
+    }  
 });
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
---
+++ js/cart2.js
@@ -0,0 +1,1 @@
+const USER_CART = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
