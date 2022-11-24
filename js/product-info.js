21be3beb1d8a057993c6bb42f502c34bc71958a0
Milagros González <110262020+gonzalezmilagros@users.noreply.github.com>
2022-11-24 15:06:07 -0300

    24/11

--- js/product-info.js
+++ js/product-info.js
@@ -130,10 +130,6 @@ document.addEventListener("DOMContentLoaded", () => {
                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-               <span class="carousel-control-next-icon" aria-hidden="true"></span>
-               <span class="visually-hidden">Next</span>
-            </button>
          </div>
       </div>
       <h4 id="pRelacionados">Productos relacionados</h4>
